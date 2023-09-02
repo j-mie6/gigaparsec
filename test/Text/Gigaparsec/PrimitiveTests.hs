eb46076fefe4e461e9076a057ea0db5c7e32ac92
Jamie Willis <j.willis19@imperial.ac.uk>
2023-09-02 18:11:50 +0100

    Further tests

--- test/Text/Gigaparsec/PrimitiveTests.hs
+++ test/Text/Gigaparsec/PrimitiveTests.hs
@@ -11,17 +11,39 @@ primitiveTests :: TestTree
 primitiveTests = testGroup "primitives"
   [ eofTests
   , pureTests
+  , emptyTests
+  , apTests
   ]
 
 eofTests :: TestTree
-eofTests = testGroup "eof should"
-  [ testCase "fail if input available" $ parse eof "a" @?= Failure
-  , testCase "succeed if input ended" $ parse eof "" @?= Success ()
-  , testCase "be pure" $ pureParse eof
-  ]
+eofTests =
+  testGroup "eof should"
+    [ testCase "fail if input available" $ parse eof "a" @?= Failure
+    , testCase "succeed if input ended" $ parse eof "" @?= Success ()
+    , testCase "be pure" $ pureParse eof
+    ]
 
 pureTests :: TestTree
-pureTests = ignoreTestBecause "not implemented" $ testGroup "pure should"
-  [ testCase "be pure" $ pureParse (pure 5)
-  , testCase "produce the given result" $ parse (pure 5) "" @?= Success 5
-  ]
+pureTests =
+  ignoreTestBecause "pure not implemented" $
+  testGroup "pure should"
+    [ testCase "be pure" $ pureParse (pure 5)
+    , testCase "produce the given result" $ parse (pure 5) "" @?= Success 5
+    ]
+
+emptyTests :: TestTree
+emptyTests =
+  ignoreTestBecause "empty not implemented" $
+  testGroup "empty should"
+    [ testCase "be pure" $ pureParse empty
+    , testCase "fail unconditionally" $ do
+        parse @() empty "" @?= Failure
+        parse @() empty "a" @?= Failure
+    ]
+
+apTests :: TestTree
+apTests = after AllSucceed "/primitives.pure/ || /primitives.empty/" $
+  ignoreTestBecause "pure, empty, and (<*>) not implemented" $
+  testGroup "(<*>) should"
+    [ testCase "" $ pureParse empty
+    ]
