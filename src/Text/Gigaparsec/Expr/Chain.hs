91ccc26b401bd7b359c23a1f5242c38a3e5345ed
Jamie Willis <j.willis19@imperial.ac.uk>
2023-10-04 10:05:03 +0100

    feat(Expr): added all the precedence machinery, but needs organisation

--- src/Text/Gigaparsec/Expr/Chain.hs
+++ src/Text/Gigaparsec/Expr/Chain.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE Safe #-}
 module Text.Gigaparsec.Expr.Chain (module Text.Gigaparsec.Expr.Chain) where
 
-import Text.Gigaparsec (Parsec)
+import Text.Gigaparsec (Parsec, (<|>))
 import Text.Gigaparsec.Expr.Infix qualified as Infix (infixl1, infixr1, infixn1, prefix, postfix)
 
 chainl1 :: Parsec a -> Parsec (a -> a -> a) -> Parsec a
@@ -18,3 +18,9 @@ prefix = Infix.prefix id
 
 postfix :: Parsec a -> Parsec (a -> a) -> Parsec a
 postfix = Infix.postfix id
+
+chainl :: Parsec a -> Parsec (a -> a -> a) -> a -> Parsec a
+chainl p op x = chainl1 p op <|> pure x
+
+chainr :: Parsec a -> Parsec (a -> a -> a) -> a -> Parsec a
+chainr p op x = chainr1 p op <|> pure x
