002f73ecf4b5b52b848782784aa70a0a10d16726
Jamie Willis <j.willis19@imperial.ac.uk>
2023-10-17 18:25:00 +0100

    test(Expr): remaining precedence tests

--- test/Text/Gigaparsec/ExprTests.hs
+++ test/Text/Gigaparsec/ExprTests.hs
@@ -20,7 +20,8 @@ import Text.Gigaparsec.Expr.InfixTests qualified as Infix
 data Data = Bin Data Data | Un Data | Unit deriving stock (Eq, Show)
 data Comp = Less Expr Expr | OfExpr Expr deriving stock (Eq, Show)
 data Expr = Add Expr Term | OfTerm Term deriving stock (Eq, Show)
-data Term = Mul Factor Term | OfFactor Factor deriving stock (Eq, Show)
+data Term = Mul Factor Term | Mul' Term Factor | OfFactor Factor
+  deriving stock (Eq, Show)
 data Factor = Neg Factor | OfAtom Atom deriving stock (Eq, Show)
 data Atom = Parens Comp | Num Int deriving stock (Eq, Show)
 
@@ -28,6 +29,10 @@ instance Subtype Expr Comp where upcast = OfExpr
 instance Subtype Term Expr where upcast = OfTerm
 instance Subtype Factor Term where upcast = OfFactor
 instance Subtype Atom Factor where upcast = OfAtom
+
+instance Subtype Atom Term where upcast = upcast @Factor . upcast
+instance Subtype Atom Expr where upcast = upcast @Term . upcast
+instance Subtype Term Comp where upcast = upcast @Expr . upcast
 
 tests :: TestTree
 tests = testGroup "Expr"
@@ -75,4 +80,39 @@ precTests = testGroup "precedence should"
       let expr = precedence' ("." $> Unit) [ ops InfixL [";" $> Bin]
                                            , ops Prefix ["~" $> Un]]
       parseAll expr "~.;." @?= Success (Un (Bin Unit Unit))
+  , testCase "generalise to sub-typed structures" do
+      let expr = precedence $  sops InfixN ["<" $> Less]
+                            +< sops InfixL ["+" $> Add]
+                            +< sops InfixR ["*" $> Mul]
+                            +< sops Prefix ["-" $> Neg]
+                            +< Atom (Num . digitToInt <$> digit <|> "(" *> (Parens <$> expr) <* ")")
+      parseAll expr "(7+8)*2+3+6*2" @?=
+        Success (upcast (Add (Add (upcast (Mul (upcast (Parens (upcast (Add (upcast (Num 7))
+                                                                            (upcast (Num 8))))))
+                                               (upcast (Num 2))))
+                                  (upcast (Num 3)))
+                             (Mul (upcast (Num 6)) (upcast (Num 2)))))
+  , testCase "generalise to non-monolithic structures" do
+      let expr = precedence $  gops InfixN OfExpr ["<" $> Less]
+                            +< gops InfixL OfTerm ["+" $> Add]
+                            +< gops InfixR OfFactor ["*" $> Mul]
+                            +< gops Prefix OfAtom ["-" $> Neg]
+                            +< Atom (Num . digitToInt <$> digit <|> "(" *> (Parens <$> expr) <* ")")
+      parseAll expr "(7+8)*2+3+6*2<4" @?=
+        Success (Less (Add (Add (upcast (Mul (upcast (Parens (upcast (Add (upcast (Num 7))
+                                                                          (upcast (Num 8))))))
+                                               (upcast (Num 2))))
+                                  (upcast (Num 3)))
+                             (Mul (upcast (Num 6)) (upcast (Num 2))))
+                      (upcast (Num 4)))
+  , testCase "generalise to non-monolithic structures with more than one chainl1" do
+      let expr = precedence $  Atom (Num . digitToInt <$> digit <|> "(" *> (Parens <$> expr) <* ")")
+                            >+ gops Prefix OfAtom ["-" $> Neg]
+                            >+ gops InfixL OfFactor ["*" $> Mul']
+                            >+ gops InfixL OfTerm ["+" $> Add]
+                            >+ gops InfixN OfExpr ["<" $> Less]
+      parseAll expr "1*(2+3)" @?=
+        Success (upcast (Mul' (upcast (Num 1))
+                              (upcast (Parens (upcast (Add (upcast (Num 2))
+                                                           (upcast (Num 3))))))))
   ]
