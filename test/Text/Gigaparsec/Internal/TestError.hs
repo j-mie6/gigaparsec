5a89dc625aeb695454517934a5b88cb7bed9c477
Jamie Willis <j.willis19@imperial.ac.uk>
2023-10-30 16:41:45 +0000

    test(Errors): implemented test instance for error builder

--- test/Text/Gigaparsec/Internal/TestError.hs
+++ test/Text/Gigaparsec/Internal/TestError.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards, TypeFamilies #-}
 module Text.Gigaparsec.Internal.TestError (
     TestError(..), TestErrorLines(..), TestErrorItem(..), parseErrorToTestError
   ) where
 
 import Text.Gigaparsec.Internal.Errors qualified as Errors
+import Text.Gigaparsec.Errors.ErrorBuilder
+import Text.Gigaparsec.Errors.Token qualified as Token
 
 import Data.Set (Set)
+import Data.List.NonEmpty (NonEmpty)
 import Data.Set qualified as Set (fromList, map)
 import Data.List.NonEmpty qualified as NonEmpty (take)
 
@@ -13,6 +16,48 @@ data TestError = TestError !(Word, Word) !TestErrorLines deriving stock (Eq, Sho
 data TestErrorLines = VanillaError !(Maybe TestErrorItem) !(Set TestErrorItem) !(Set String) !Word
                     | SpecialisedError !(Set String) !Word deriving stock (Eq, Show, Ord)
 data TestErrorItem = Raw !String | Named !String | EndOfInput deriving stock (Eq, Show, Ord)
+
+instance ErrorBuilder TestError where
+  format p _ = TestError p
+
+  type Position TestError = (Word, Word)
+  pos = (,)
+
+  type Source TestError = ()
+  source = const ()
+
+  type ErrorInfoLines TestError = TestErrorLines
+  vanillaError = VanillaError
+  specialisedError = SpecialisedError
+
+  type ExpectedItems TestError = Set TestErrorItem
+  combineExpectedItems = id
+
+  type Messages TestError = Set String
+  combineMessages = Set.fromList
+
+  type UnexpectedLine TestError = Maybe TestErrorItem
+  unexpected = id
+  type ExpectedLine TestError = Set TestErrorItem
+  expected = id
+
+  type Message TestError = String
+  reason = id
+  message = id
+
+  type LineInfo TestError = Word
+  lineInfo _ _ _ _ width = width
+
+  numLinesBefore = 2
+  numLinesAfter = 2
+
+  type Item TestError = TestErrorItem
+  raw = Raw
+  named = Named
+  endOfInput = EndOfInput
+
+  unexpectedToken :: NonEmpty Char -> Word -> Bool -> Token.Token
+  unexpectedToken cs demanded _ = Token.Raw (NonEmpty.take (fromIntegral demanded) cs)
 
 parseErrorToTestError :: Errors.ParseError -> TestError
 parseErrorToTestError Errors.VanillaError{..} =
