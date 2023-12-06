66b7cd02dec00e71896abb37b8443c9880eaeea0
Jamie Willis <j.willis19@imperial.ac.uk>
2023-12-06 00:30:06 +0000

    feat(Token): added remaining description

--- src/Text/Gigaparsec/Token/Descriptions.hs
+++ src/Text/Gigaparsec/Token/Descriptions.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE Safe #-}
 {-# LANGUAGE OverloadedLists #-}
+{-# OPTIONS_GHC -Wno-partial-fields #-}
 module Text.Gigaparsec.Token.Descriptions (module Text.Gigaparsec.Token.Descriptions) where
 
 import Data.Char (isSpace)
 import Data.Set (Set)
+import Data.Map (Map)
+import Data.List.NonEmpty (NonEmpty)
 
 type LexicalDesc :: *
-data LexicalDesc = LexicalDesc { nameDesc :: !NameDesc
-                               , symbolDesc :: !SymbolDesc
-                               , numericDesc :: !NumericDesc
-                               , textDesc :: !TextDesc
-                               , spaceDesc :: !SpaceDesc
+data LexicalDesc = LexicalDesc { nameDesc :: {-# UNPACK #-} !NameDesc
+                               , symbolDesc :: {-# UNPACK #-} !SymbolDesc
+                               , numericDesc :: {-# UNPACK #-} !NumericDesc
+                               , textDesc :: {-# UNPACK #-} !TextDesc
+                               , spaceDesc :: {-# UNPACK #-} !SpaceDesc
                                }
 
 plain :: LexicalDesc
@@ -48,16 +51,139 @@ plainSymbol = SymbolDesc { hardKeywords = []
                          }
 
 type NumericDesc :: *
-data NumericDesc = NumericDesc {}
+data NumericDesc = NumericDesc { literalBreakChar :: !BreakCharDesc
+                               , leadingDotAllowed :: !Bool
+                               , trailingDotAllowed :: !Bool
+                               , leadingZerosAllowed :: !Bool
+                               , positiveSign :: !PlusSignPresence
+                               -- generic number
+                               , integerNumbersCanBeHexadecimal :: !Bool
+                               , integerNumbersCanBeOctal :: !Bool
+                               , integerNumbersCanBeBinary :: !Bool
+                               , realNumbersCanBeHexadecimal :: !Bool
+                               , realNumbersCanBeOctal :: !Bool
+                               , realNumbersCanBeBinary :: !Bool
+                               -- special literals
+                               , hexadecimalLeads :: !(Set Char)
+                               , octalLeads :: !(Set Char)
+                               , binaryLeads :: !(Set Char)
+                               -- exponents
+                               , decimalExponentDesc :: !ExponentDesc
+                               , hexadecimalExponentDesc :: !ExponentDesc
+                               , octalExponentDesc :: !ExponentDesc
+                               , binaryExponentDesc :: !ExponentDesc
+                               }
 
 plainNumeric :: NumericDesc
-plainNumeric = NumericDesc {}
+plainNumeric = NumericDesc { literalBreakChar = NoBreakChar
+                           , leadingDotAllowed = False
+                           , trailingDotAllowed = False
+                           , leadingZerosAllowed = True
+                           , positiveSign = PlusOptional
+                           -- generic number
+                           , integerNumbersCanBeHexadecimal = True
+                           , integerNumbersCanBeOctal = True
+                           , integerNumbersCanBeBinary = False
+                           , realNumbersCanBeHexadecimal = False
+                           , realNumbersCanBeOctal = False
+                           , realNumbersCanBeBinary = False
+                           -- special literals
+                           , hexadecimalLeads = ['x', 'X']
+                           , octalLeads = ['o', 'O']
+                           , binaryLeads = ['b', 'B']
+                           -- exponents
+                           , decimalExponentDesc = ExponentsSupported { compulsory = False
+                                                                      , chars = ['e', 'E']
+                                                                      , base = 10
+                                                                      , expSign = PlusOptional
+                                                                      }
+                           , hexadecimalExponentDesc = ExponentsSupported { compulsory = True
+                                                                          , chars = ['p', 'P']
+                                                                          , base = 2
+                                                                          , expSign = PlusOptional
+                                                                          }
+                           , octalExponentDesc = ExponentsSupported { compulsory = True
+                                                                    , chars = ['e', 'E', 'p', 'P']
+                                                                    , base = 2
+                                                                    , expSign = PlusOptional
+                                                                    }
+                           , binaryExponentDesc = ExponentsSupported { compulsory = True
+                                                                     , chars = ['e', 'E', 'p', 'P']
+                                                                     , base = 2
+                                                                     , expSign = PlusOptional
+                                                                     }
+                           }
+
+type ExponentDesc :: *
+data ExponentDesc = NoExponents
+                  | ExponentsSupported { compulsory :: !Bool
+                                       , chars :: !(Set Char)
+                                       , base :: !Int
+                                       , expSign :: !PlusSignPresence
+                                       }
+
+type BreakCharDesc :: *
+data BreakCharDesc = NoBreakChar
+                   | BreakCharSupported { breakChar :: !Char
+                                        , allowedAfterNonDecimalPrefix :: !Bool
+                                        }
+
+type PlusSignPresence :: *
+data PlusSignPresence = PlusRequired | PlusOptional | PlusIllegal
 
 type TextDesc :: *
-data TextDesc = TextDesc {}
+data TextDesc = TextDesc { escapeSequences :: {-# UNPACK #-} !EscapeDesc
+                         , characterLiteralEnd :: !Char
+                         , stringEnds :: !(Set String)
+                         , multiStringEnds :: !(Set String)
+                         , graphicCharacter :: !CharPredicate
+                         }
 
 plainText :: TextDesc
-plainText = TextDesc {}
+plainText = TextDesc { escapeSequences = plainEscape
+                     , characterLiteralEnd = '\''
+                     , stringEnds = ["\""]
+                     , multiStringEnds = []
+                     , graphicCharacter = Just (>= ' ')
+                     }
+
+type EscapeDesc :: *
+data EscapeDesc = EscapeDesc { escBegin :: !Char
+                             , literals :: !(Set Char)
+                             , singleMap :: !(Map Char Char)
+                             , multiMap :: !(Map String Char)
+                             , decimalEscape :: !NumericEscape
+                             , hexadecimalEscape :: !NumericEscape
+                             , octalEscape :: !NumericEscape
+                             , binaryEscape :: !NumericEscape
+                             , emptyEscape :: !(Maybe Char)
+                             , gapsSupported :: !Bool
+                             }
+
+plainEscape :: EscapeDesc
+plainEscape = EscapeDesc { escBegin = '\\'
+                         , literals = ['\\']
+                         , singleMap = []
+                         , multiMap = []
+                         , decimalEscape = NumericIllegal
+                         , hexadecimalEscape = NumericIllegal
+                         , octalEscape = NumericIllegal
+                         , binaryEscape = NumericIllegal
+                         , emptyEscape = Nothing
+                         , gapsSupported = False
+                         }
+
+-- TODO: haskellEscape
+
+type NumericEscape :: *
+data NumericEscape = NumericIllegal
+                   | NumericSupported { prefix :: !(Maybe Char)
+                                      , numDigits :: !NumberOfDigits
+                                      , maxValue :: !Int
+                                      }
+
+type NumberOfDigits :: *
+data NumberOfDigits = Unbounded | Exactly !(NonEmpty Word) | AtMost !Word
 
 type SpaceDesc :: *
 data SpaceDesc = SpaceDesc { commentStart :: !String
