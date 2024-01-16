f43aee87a0c8844994b9b938d21be84ee48577e3
Jamie Willis <j.willis19@imperial.ac.uk>
2024-01-16 00:01:04 +0000

    feat: start of text parsers

---
+++ src/Text/Gigaparsec/Token/Text.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE Safe #-}
+-- TODO: refine, move to Internal
+module Text.Gigaparsec.Token.Numeric (module Text.Gigaparsec.Token.Numeric) where
+
+import Text.Gigaparsec (Parsec, void, (<|>), empty, filterS)
+import Text.Gigaparsec.Char (char, digit, hexDigit, octDigit, bit, satisfy)
+import Text.Gigaparsec.Token.Descriptions (TextDesc(..), EscapeDesc(..), NumericEscape)
+import Text.Gigaparsec.Token.Generic (GenericNumeric(zeroAllowedDecimal, zeroAllowedHexadecimal, zeroAllowedOctal, zeroAllowedBinary))
+
+type TextParsers :: * -> *
+data TextParsers t = TextParsers { unicode :: Parsec t
+                                 , ascii :: Parsec t
+                                 , latin1 :: Parsec t
+                                 }
+
+-- I want the convenient naming, sue me
+type StringParsers :: *
+type StringParsers = TextParsers String
+
+type CharacterParsers :: *
+type CharacterParsers = TextParsers Char
+
+mkCharacterParsers :: TextDesc -> Escape -> CharacterParsers
+mkCharacterParsers TextDesc{..} escape = TextParsers {..}
+  where unicode = lit uncheckedUniLetter
+        ascii = lit (filterS (<= '\x7f') uncheckedUniLetter)
+        latin1 = lit (filterS (<= '\xff') uncheckedUniLetter)
+
+        quote = char characterLiteralEnd
+        lit c = quote *> c <* quote
+        uncheckedUniLetter = escapeChar escape <|> graphic
+
+        -- FIXME: nope!
+        graphic = maybe empty satisfy graphicCharacter
+
+type Escape :: *
+data Escape = Escape { escapeCode :: !(Parsec Char)
+                     , escapeBegin :: !(Parsec ())
+                     , escapeChar :: !(Parsec Char)
+                     }
+
+mkEscape :: EscapeDesc -> GenericNumeric -> Escape
+mkEscape EscapeDesc{..} gen = Escape {..}
+  where
+    escapeBegin = void (char escBegin)
+    escapeCode = escMapped <|> numericEscape
+    escapeChar = escapeBegin *> escapeCode
+
+    escMapped = undefined --TODO:
+    numericEscape = decimalEsc <|> hexadecimalEsc <|> octalEsc <|> binaryEsc
+
+    decimalEsc = fromDesc 10 decimalEscape (zeroAllowedDecimal gen) digit
+    hexadecimalEsc = fromDesc 16 hexadecimalEscape (zeroAllowedHexadecimal gen) hexDigit
+    octalEsc = fromDesc 8 octalEscape (zeroAllowedOctal gen) octDigit
+    binaryEsc = fromDesc 2 binaryEscape (zeroAllowedBinary gen) bit
+
+    fromDesc :: Int -> NumericEscape -> Parsec Integer -> Parsec Char -> Parsec Char
+    fromDesc radix _ integer digit = undefined --TODO:
+
+lexemeText :: (forall a. Parsec a -> Parsec a) -> TextParsers t -> TextParsers t
+lexemeText lexe TextParsers{..} = TextParsers {
+    unicode = lexe unicode,
+    ascii = lexe ascii,
+    latin1 = lexe latin1
+  }
