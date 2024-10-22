a6c35f4241eeab33551258892a2e45a85164f273
David Davies <dd2517@ic.ac.uk>
2024-10-22 17:09:19 +0100

    docs(Expr.Infix): add section headers

--- src/Text/Gigaparsec/Expr/Infix.hs
+++ src/Text/Gigaparsec/Expr/Infix.hs
@@ -16,9 +16,19 @@ Compared with the combinators in 'Text.Gigaparsec.Expr.Chain',
 these allow for more freedom in the type of the values and the operators.
 -}
 module Text.Gigaparsec.Expr.Infix (
+  -- ** Binary Operator Chains
+  {-|
+  These combinators allow for the chaining together of values and binary operators in either left-, 
+  right- or non-associative application.
+  -}
   infixl1,
   infixr1,
   infixn1,
+  -- ** Unary Operator Chains
+  {-|
+  These combinators allow for the chaining together, and application, of multiple 
+  prefix or postfix unary operators to a single value.
+  -}
   prefix,
   postfix,
   -- ** Module Re-export
@@ -36,7 +46,7 @@ __zero__ or more binary operators between __one__ or more values.
 First parse @p@, then parse @op@ followed by a @p@ repeatedly. 
 The results of the @p@s, @x₁@ through @xₙ@, are combined with the results of the @op@s, 
 @f₁@ through @fₙ₋₁@, with left-associative application: 
-fₙ₋₁(fₙ₋₂(..f₁(x₁, x₂).., xₙ₋₁), xₙ).
+fₙ₋₁ (fₙ₋₂ (..(f₁ x₁ x₂)..) xₙ₋₁) xₙ.
 This application is then returned as the result of the combinator. 
 If @p@ or @op@ fails having consumed input at any point, the whole combinator fails.
 
@@ -59,7 +69,7 @@ __zero__ or more binary operators between __one__ or more values.
 First parse @p@, then parse @op@ followed by a @p@ repeatedly. 
 The results of the @p@s, @x₁@ through @xₙ@, are combined with the results of the @op@s, 
 @f₁@ through @fₙ@, with right-associative application: 
-f₁(x₁,f₂(x₂,...fₙ₋₁(xₙ₋₁, xₙ)..)).
+@f₁ x₁ (f₂ x₂ (...(fₙ₋₁ xₙ₋₁ xₙ)...))@.
 This application is then returned as the result of the combinator. 
 If @p@ or @op@ fails having consumed input at any point, the whole combinator fails.
 
@@ -79,11 +89,18 @@ infixr1 wrap p op = p <**> (flip <$> op <*> infixr1 wrap p op <|> pure wrap)
 This combinator handles non-associative parsing, and the application of, 
 __zero__ or __one__ binary operators between __one__ or __two__ values.
 
-First parse @p@, then parse @op@ followed by a single @p@. 
-The results of the @p@s, @x@ and @y@, are combined with the result of @op@, 
-@f@ with the application @f(x,y)@.
-This application is then returned as the result of the combinator. 
+First parse @p@.
+Then:
+
+* If this not is followed by an @op@, simply return @p@.
+* Otherwise, parse this @op@ followed by a single @p@. 
+  Then ensure that this is not followed by a further @op@, to enforce non-associativity.
+  The results of the @p@s, @x@ and @y@, are combined with the result of @op@, 
+  @f@ with the application @f x y@.
+  This application is then returned as the result of the combinator. 
+
 If @p@ or @op@ fails having consumed input at any point, the whole combinator fails.
+This combinator also fails if the second @p@ is followed by another @op@.
 -}
 infixn1 :: (a -> b)             -- ^ a function converting the value type @a@ into the result type @b@.
         -> Parsec a             -- ^ @p@, the value to be parsed
@@ -99,7 +116,7 @@ __zero__ or more prefix unary operators to a single value.
 First parse many repeated @op@s. 
 When there are no more @op@s left to parse, parse a single @p@. 
 The result of @p@, @x@, is applied first to @wrap@, and then to each of the results of the @op@s, @f₁@ through @fₙ@, 
-such that @fₙ@ is applied first and @f₁@ last: @f₁(f₂(..fₙ(wrap x)..))@. 
+such that @fₙ@ is applied first and @f₁@ last: @f₁ (f₂ (..(fₙ (wrap x))..))@. 
 This application is then returned as the result of the combinator. 
 
 If @p@ or @op@ fails having consumed input at any point, the whole combinator fails.
@@ -118,7 +135,7 @@ __zero__ or more postfix unary operators to a single value.
 First parse a single @p@.
 Then, parse many repeated @op@s.
 The result of @p@, @x@, is applied first to @wrap@, and then to each of the results of the @op@s, @f₁@ through @fₙ@, 
-such that @f₁@ is applied first and @fₙ@ last: @fₙ(fₙ₋₁(..f₁(wrap x)..))@. 
+such that @f₁@ is applied first and @fₙ@ last: @fₙ( fₙ₋₁(..f₁ (wrap x)..))@. 
 This application is then returned as the result of the combinator. 
 
 If @p@ or @op@ fails having consumed input at any point, the whole combinator fails.
